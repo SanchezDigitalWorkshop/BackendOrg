ffb481e2e949bc701dd3fcea65fb6a0ab9622d6e
SanchezDigitalWorkshop <SanchezDigitalWorkshop@outlook.com>
2024-11-12 03:09:18 -0500

    Update InmuebleController.java

--- src/main/java/com/example/demos/controller/InmuebleController.java
+++ src/main/java/com/example/demos/controller/InmuebleController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/inmuebles")
-
+@CrossOrigin(origins = "*")
 public class InmuebleController {
 
     @Autowired
