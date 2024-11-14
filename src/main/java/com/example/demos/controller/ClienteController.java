c3b3883316e97d26dce8d078b8f3198d05d853e2
SanchezDigitalWorkshop <SanchezDigitalWorkshop@outlook.com>
2024-11-14 13:29:32 -0500

    di
    di

--- src/main/java/com/example/demos/controller/ClienteController.java
+++ src/main/java/com/example/demos/controller/ClienteController.java
@@ -1,6 +1,7 @@
 package com.example.demos.controller;
 
 import com.example.demos.model.Cliente;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
