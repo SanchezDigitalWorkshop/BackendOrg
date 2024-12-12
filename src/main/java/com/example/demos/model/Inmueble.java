89c091f08d120747203fd7e3b3f854dc3059f14b
SanchezDigitalWorkshop <SanchezDigitalWorkshop@outlook.com>
2024-12-11 21:47:10 -0500

    a
    a

--- src/main/java/com/example/demos/model/Inmueble.java
+++ src/main/java/com/example/demos/model/Inmueble.java
@@ -14,7 +14,7 @@ public class Inmueble {
     private String id;
     private String tipoOperacion; // Venta o Alquiler
     private String tipoPropiedad; // Casa, Departamento, etc.
-    private String tipoProyecto; // En plano, En construcción,  
+    private String tipoProyecto; // En plano, En construc   
     private Ubicacion ubicacion;
     private Caracteristicas caracteristicas;
     private Precio precio;
