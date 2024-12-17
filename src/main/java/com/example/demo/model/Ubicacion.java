7c806c9c0967ae94890870d3e95de9129d4f9a47
SanchezDigitalWorkshop <SanchezDigitalWorkshop@outlook.com>
2024-12-16 20:13:10 -0500

    a
    aaa

--- src/main/java/com/example/demo/model/Ubicacion.java
+++ src/main/java/com/example/demo/model/Ubicacion.java
@@ -9,6 +9,6 @@ public class Ubicacion {
     private String departamento;
     private String provincia;
     private String formaUbicacion; // Exacta o Aproximada
-    private String logitud;
+    private String longitud;
     private String latitud;
 }
