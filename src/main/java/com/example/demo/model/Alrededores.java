8416cea837853369eb79cf252b9462ff2b698672
SanchezDigitalWorkshop <SanchezDigitalWorkshop@outlook.com>
2024-12-17 23:43:23 -0500

    Revert "package com.example.demo.model;  import lombok.Data;  @Data public class Servicio {     private boolean agua = false;     private boolean electricidad = false;     private boolean gasNatural = false;     private boolean internet = false;     private boolean telefono = false;     private boolean televisionCable = false;     private boolean aireAcondicionado = false;     private boolean calefaccion = false;     private boolean ascensor = false;     private boolean piscina = false;     private boolean gimnasio = false;     private boolean seguridad24Horas = false;     private boolean estacionamiento = false;     private boolean accesoDiscapacidad = false;      /**      * Método para completar valores predeterminados si algún campo es nulo o no está asignado.      */     public void completarDatosPorDefecto() {         this.agua = this.agua;         this.electricidad = this.electricidad;         this.gasNatural = this.gasNatural;         this.internet = this.internet;         this.telefono = this.telefono;         this.televisionCable = this.televisionCable;         this.aireAcondicionado = this.aireAcondicionado;         this.calefaccion = this.calefaccion;         this.ascensor = this.ascensor;         this.piscina = this.piscina;         this.gimnasio = this.gimnasio;         this.seguridad24Horas = this.seguridad24Horas;         this.estacionamiento = this.estacionamiento;         this.accesoDiscapacidad = this.accesoDiscapacidad;     } }"
    This reverts commit f9861fda149a375149db9307d17da660342b6d7e.

--- src/main/java/com/example/demo/model/Alrededores.java
+++ src/main/java/com/example/demo/model/Alrededores.java
@@ -4,34 +4,16 @@ import lombok.Data;
 
 @Data
 public class Alrededores {
-    private boolean cercaParques = false;
-    private boolean cercaColegios = false;
-    private boolean cercaHospitales = false;
-    private boolean cercaCentrosComerciales = false;
-    private boolean cercaSupermercados = false;
-    private boolean cercaTransportePublico = false;
-    private boolean zonaSegura = false;
-    private boolean zonaTranquila = false;
-    private boolean areaComercial = false;
-    private boolean areaResidencial = false;
-    private boolean vistaAlMar = false;
-    private boolean vistaALaCiudad = false;
-
-    /**
-     * Método para completar los valores por defecto si algún campo está nulo.
-     */
-    public void completarDatosPorDefecto() {
-        this.cercaParques = this.cercaParques;
-        this.cercaColegios = this.cercaColegios;
-        this.cercaHospitales = this.cercaHospitales;
-        this.cercaCentrosComerciales = this.cercaCentrosComerciales;
-        this.cercaSupermercados = this.cercaSupermercados;
-        this.cercaTransportePublico = this.cercaTransportePublico;
-        this.zonaSegura = this.zonaSegura;
-        this.zonaTranquila = this.zonaTranquila;
-        this.areaComercial = this.areaComercial;
-        this.areaResidencial = this.areaResidencial;
-        this.vistaAlMar = this.vistaAlMar;
-        this.vistaALaCiudad = this.vistaALaCiudad;
-    }
+    private boolean cercaParques;
+    private boolean cercaColegios;
+    private boolean cercaHospitales;
+    private boolean cercaCentrosComerciales;
+    private boolean cercaSupermercados;
+    private boolean cercaTransportePublico;
+    private boolean zonaSegura;
+    private boolean zonaTranquila;
+    private boolean areaComercial;
+    private boolean areaResidencial;
+    private boolean vistaAlMar;
+    private boolean vistaALaCiudad;
 }
